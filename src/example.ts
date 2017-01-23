1c4b172016e6fc844f210eaa1f82e0a3518bce6c
Jim Bolla <github@jimbolla.com>
2017-01-22 22:57:17 -0500

    Squashed 'redux-reducer-effects/' content from commit 14b5575
    git-subtree-dir: redux-reducer-effects
    git-subtree-split: 14b5575930e75936dfcf9fe1d06454b1de789c81

---
+++ src/example.ts
@@ -0,0 +1,108 @@
+import enhance, {TaskRunner} from './redux-reducer-effects';
+import {createStore} from 'redux';
+import { Subject, Observable } from "@reactivex/rxjs";
+
+// Helpers
+const create = <T>(t: T): T => t;
+export type Success<A> = { success: true; value: A; }
+export type Failure<X> = { success: false; value: X }
+export type Result<X, A> = Failure<X> | Success<A>;
+export const createFailure = <X>(x: X): Failure<X> => ({ success: false, value: x })
+export const createSuccess = <A>(a: A): Success<A> => ({ success: true, value: a })
+const patch = <O, P>(o: O, p: P): O & P => Object.assign({}, o, p);
+
+// Actions
+enum ActionTypes { Fetch, FetchSuccess, FetchError };
+type FetchAction = { type: ActionTypes.Fetch };
+const createFetchAction = (): FetchAction => ({ type: ActionTypes.Fetch })
+type FetchSuccessAction = { type: ActionTypes.FetchSuccess, result: Success<string> };
+const createFetchSuccessAction = (result: Success<string>): FetchSuccessAction => ({
+    type: ActionTypes.FetchSuccess,
+    result
+});
+type FetchErrorAction = { type: ActionTypes.FetchError, result: Failure<string> };
+const createFetchErrorAction = (result: Failure<string>): FetchErrorAction => ({
+    type: ActionTypes.FetchError,
+    result
+});
+type Action = FetchAction | FetchSuccessAction | FetchErrorAction;
+
+// Tasks
+type GetRandomGifTask = {
+    type: 'GetRandomGif',
+    topic: string,
+    onFail: (x: Failure<string>) => FetchErrorAction,
+    onSuccess: (a: Success<string>) => FetchSuccessAction,
+}
+type Task = GetRandomGifTask;
+
+type State = {
+    status: 'not started' | 'pending' | 'success' | 'error',
+    result?: Result<string, string>
+}
+const reducer = (state: State, action: Action): [State, Task[]] => {
+    switch (action.type) {
+        case ActionTypes.Fetch:
+            return [patch(state, { status: 'pending', result: undefined }), [
+                {
+                    type: 'GetRandomGif',
+                    topic: 'food',
+                    onFail: createFetchErrorAction,
+                    onSuccess: createFetchSuccessAction,
+                }
+            ]];
+        case ActionTypes.FetchSuccess:
+            return [patch(state, { status: 'success', result: action.result }), []]
+        case ActionTypes.FetchError:
+            return [patch(state, { status: 'error', result: action.result }), []]
+        default:
+            return [state, []];
+    }
+}
+
+const initialState: State = {
+    status: 'not started'
+};
+
+const createGifUrl = (topic: string): string => `https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=${topic}`;
+const decodeGifUrl = (response: any): string => response.data.image_url;
+const myTaskRunner: TaskRunner<Task, Action> = (tasks$: Observable<Task>): Observable<Action> => {
+    return tasks$
+        .filter(task => task.type === 'GetRandomGif')
+        .switchMap(task => {
+            const url = createGifUrl(task.topic);
+            return Observable.ajax({ url, crossDomain: true })
+                .map(response => response.response)
+                .map(decodeGifUrl)
+                .map(createSuccess)
+                .catch(error => Observable.of(createFailure(error)))
+                .map(result => (
+                    result.success
+                        ? task.onSuccess(result)
+                        : task.onFail(result)
+                ))
+        })
+};
+
+const enhancedCreateStore = enhance({
+    createSubject: () => new Subject<Task>(),
+    taskRunner: myTaskRunner,
+})(createStore);
+const store = enhancedCreateStore(reducer, initialState);
+
+const rootEl = document.getElementById('root');
+store.subscribe(() => {
+    const state = store.getState();
+    if (rootEl) rootEl.innerHTML = `
+<pre>
+Status: ${state.status}
+Result success: ${state.result !== undefined ? state.result.success : ''}
+Result success value/failure reason: ${state.result !== undefined
+    ? state.result.success === true
+        ? JSON.stringify(state.result.value, null, '\t')
+        : state.result
+    : ''}
+</pre>`;
+});
+
+store.dispatch(createFetchAction());
---
+++ src/test.ts
@@ -0,0 +1,118 @@
+import enhance, { composeReducers, EnhancedReducerResult, getState, getTasks } from "./redux-reducer-effects";
+import { assert } from "chai";
+import { createStore } from "redux";
+import { Subject, Observable, Scheduler } from "@reactivex/rxjs";
+
+const create = <T>(t: T): T => t;
+
+type IncrementAction = { type: 'increment' };
+type AsyncIncrementAction = { type: 'asyncInc' };
+type Action = IncrementAction | AsyncIncrementAction;
+type AsyncIncrementTask = { task: 'asyncInc' };
+type StubTask = { task: 'do thing' };
+type Task = AsyncIncrementTask | StubTask;
+type State = {
+    counter: number,
+};
+
+describe("redux-reducer-effects", function() {
+
+    describe("enhance store", function() {
+
+        it("can create", function() {
+
+
+            const enhancerStack = enhance({
+                createSubject: () => new Subject<Task>(),
+                taskRunner,
+            });
+
+            const enhancedCreateStore = enhancerStack(createStore);
+            const store = enhancedCreateStore(reducer, { counter: 0 }, enhancerStack);
+
+            store.dispatch({ type: "asyncInc" });
+
+            return wait(5)
+                .then(() => {
+                    assert.deepEqual(store.getState(), { counter: 1 });
+                })
+
+            function reducer(state: State, action: Action): EnhancedReducerResult<State,Task> {
+                switch(action.type) {
+                    case "asyncInc":
+                        return [state, { task: "asyncInc" }];
+
+                    case "increment":
+                        return { counter: state.counter + 1 };
+
+                    default:
+                        return state;
+                }
+            }
+
+            function taskRunner(task$: Observable<Task>): Observable<Action> {
+                return task$
+                    .map(() => (create<IncrementAction>({ type: "increment" })))
+                    .observeOn(Scheduler.async)
+            }
+
+
+        })
+
+    })
+
+    describe("compose reducers", function() {
+
+        it("keeps all state changes", function() {
+            const combined = composeReducers(increment, increment, increment);
+
+            const result = combined({ counter: 0 }, { type: "init" });
+            const state = getState(result);
+            assert(state, "expected final state");
+            assert.equal(state.counter, 3);
+
+            function increment(s: State, action: Action) {
+                return {
+                    counter: s.counter + 1,
+                }
+            }
+        })
+
+        it("combines tasks, from both states with and without new tasks", function() {
+            const combined = composeReducers(increment, incrementWithTask, increment, incrementWithTask);
+            const stubTask = () => create<StubTask>({ task: "do thing" });
+
+            const result = combined({ counter: 0 }, { type: "init" });
+            const state = getState(result);
+            const tasks = getTasks(result);
+
+            assert(state, "expected final state");
+            assert.equal(state.counter, 4);
+            assert.deepEqual(tasks, [stubTask(), stubTask()]);
+
+            function increment(s: State, action: Action) {
+                return {
+                    counter: s.counter + 1,
+                }
+            }
+
+            function incrementWithTask(s: State, action: Action): EnhancedReducerResult<State, Task> {
+                return [{
+                    counter: s.counter + 1,
+                }, stubTask()]
+            }
+        })
+
+
+    })
+
+    describe("combine reducers", function() {
+
+
+    })
+})
+
+
+function wait(n: number) {
+    return new Promise(resolve => setTimeout(resolve, n));
+}
